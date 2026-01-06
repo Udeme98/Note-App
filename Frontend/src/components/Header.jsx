fe69b70f7e9723a54cb204a8f6528db10e318b21
Udeme Udeme <darealudeme1@gmail.com>
2026-01-06 09:34:35 +0100

    log out functionality

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import toast from "react-hot-toast";
 
 export default function Header({ rightButton }) {
   const { user, logout } = useAuth();
@@ -7,6 +8,7 @@ export default function Header({ rightButton }) {
 
   const handleLogout = () => {
     logout();
+    toast.success("Logged out successfully");
     navigate("/");
   };
 
