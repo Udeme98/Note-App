4ea6e679457f595c8035685e5ca9389c8ac313c2
Udeme Udeme <darealudeme1@gmail.com>
2026-01-06 14:22:58 +0100

    Update API URL for production

--- Frontend/src/api/axios.jsx
+++ Frontend/src/api/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  // baseURL: "http://localhost:3001/api",
+  baseURL: "https://note-app-backend-901l.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
